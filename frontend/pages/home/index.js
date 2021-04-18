a2ea2f8958db15fc51976be293d20ac1d63b73f8
reechee <richardokonicha@gmail.com>
2021-04-18 08:28:30 +0100

    add material

---
+++ frontend/pages/home/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import { Container } from '@material-ui/core'
+import Copyright from '../../src/components/Copyright'
+import Header from '../../src/components/Header'
+
+
+export default function Home(){
+    return(
+        <>
+        <Header/>
+        <Container>
+            
+        this ios a test
+        <Copyright/>
+        </Container>
+        </>
+    )
+}

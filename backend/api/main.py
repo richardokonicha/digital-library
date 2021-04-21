02a612a313e63b466087de42b4b3fb21bbcecf24
reechee <richardokonicha@gmail.com>
2021-04-21 18:21:57 +0100

    complete fast api auth

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,25 +1,36 @@
 import json
+from typing import Optional
 from starlette.requests import Request
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import HTMLResponse
-from fastapi import FastAPI
+import httpx
+
+import jwt
+from jwt import PyJWTError
+
+
+from fastapi import FastAPI, Depends, Security
+from fastapi.security import OAuth2AuthorizationCodeBearer
+
 from api.routers.auth import router as AuthRouter
 from api.routers.users import router as UserRouter
 
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="!secret")
-# app.add_middleware(
-#     CORSMiddleware,
-#     SessionMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"]
-# )
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
 
 app.include_router(AuthRouter, tags=["Authentication"], prefix="/authentication")
 app.include_router(UserRouter, tags=["Users"], prefix="/user")
+
+authorizationUrl = "https://accounts.google.com/o/oauth2/v2/auth"
+tokenUrl = "https://oauth2.googleapis.com/token"
+
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl=authorizationUrl, tokenUrl=tokenUrl, auto_error=True, scopes={"openid":"", "email":"", "profile":""})
 
 
 @app.get('/')
@@ -35,6 +46,25 @@ async def homepage(request: Request):
     return HTMLResponse('<a href="/authentication/login">login</a>')
 
 
-@app.get("/items")
-def read_item():
-    return {"hello": "world"}
+@app.get("/get_user")
+async def read_item(token: Optional[str] = Security(oauth2_scheme)):
+    userurl = "https://openidconnect.googleapis.com/v1/userinfo"
+
+    headers = {
+        'scope': "openid profile email",
+        'accept': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+    r = httpx.get(userurl, headers=headers)
+    return {"hello": r.json()}
+
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     SessionMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
--- backend/api/routers/auth.py
+++ backend/api/routers/auth.py
@@ -26,18 +26,22 @@ oauth.register(
 @router.get('/login')
 async def login(request: Request):  
     redirect_uri = request.url_for('auth')
-    return await oauth.google.authorize_redirect(request, redirect_uri)
+    print(redirect_uri)
+    newurl = await oauth.google.authorize_redirect(request, redirect_uri)
+    return newurl
 
 
 @router.get('/auth')
 async def auth(request: Request):
+    print(request)
     try:
         token = await oauth.google.authorize_access_token(request)
     except OAuthError as error:
         return HTMLResponse(f'<h1>{error.error}</h1>')
     user = await oauth.google.parse_id_token(request, token)
     request.session['user'] = dict(user)
-    return RedirectResponse(url='/')
+    # return RedirectResponse(url='/')
+    return {"access_token": token, "token_type": "bearer"}
 
 
 @router.get('/logout')
